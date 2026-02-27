d9f6d5376f363cc443619397b6d17540f5f57529
devansh1916 <212507290+devansh1916@users.noreply.github.com>
2026-02-27 10:01:47 +0530

    feat:establish postgres node js connection

---
+++ src/db.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const {Pool} =require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,      
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.on('error',(err,client)=> {
+    console.log('Unexpected Error',err)
+    process.exit(-1);
+})
+
+async function CheckConnection() {
+    try {
+        const res= await pool.query('SELECT NOW()');
+        console.log("Connection Succesful");
+        console.log(res.rows[0]);
+    }
+    catch (e) {
+        console.log("Connection failed",e.stack);
+    }
+}
+
+CheckConnection();

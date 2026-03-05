8f3acbe1f60894a0d896b904af6144cf2c0aa56d
devansh1916 <212507290+devansh1916@users.noreply.github.com>
2026-03-05 21:13:19 +0530

    feat:implemeented booking logic for concurrency handling

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const db = require('./db');
+import express from 'express';
+
+import db, { Client } from './db.js';
+db.pool.query('SELECT NOW()');
 
 const app= express();
 
@@ -12,12 +14,49 @@ app.get('/check',(req,res) => {
     })
 })
 
-app.post('/api/book',(req,res) => {
+app.post('/api/book',async (req,res) => {
     const {userID , eventID} =req.body;
-    res.json({
-        message: "Booking initiated",
-        recievedData: {userID,eventID}
-    })
+    const client= await db.pool.connect();
+    try {
+        await client.query('BEGIN');
+
+        const findQuery=` 
+            SELECT id, row_letter, seat_number 
+            FROM seats 
+            WHERE event_id = $1 AND status = 'AVAILABLE' 
+            LIMIT 1 
+            FOR UPDATE SKIP LOCKED
+        `;
+        const { rows } = await client.query(findQuery,[eventID]);
+        if (rows.length === 0) {
+            await client.query('ROLLBACK'); // Pop the bubble
+            return res.status(404).json({ error: "Sorry, this event is sold out!" });
+        }
+        const seat=rows[0];
+
+        const upadateQuery=`
+        UPDATE seats
+        SET status='SOLD' ,holder_id=$1
+        WHERE id=$2
+        RETURNING *
+        ` 
+        await client.query(upadateQuery,[userID,seat.id]);
+        
+        await client.query('COMMIT');
+
+        res.json({
+            message: "🎟️ Ticket booked successfully!",
+            seat: `Row ${seat.row_letter}, Seat ${seat.seat_number}`
+        });
+    }
+    catch(error) {
+        await client.query('ROLLBACK');
+        console.log('Booking error:',error);
+        res.status(500).json({ error: "Internal server error during booking." });
+    }
+    finally {
+        client.release();
+    }
 })
 
 const PORT= process.env.API_PORT || 3000;

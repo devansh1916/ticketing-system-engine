d553f598583f87aa30854e4304386597576bdcd8
devansh1916 <212507290+devansh1916@users.noreply.github.com>
2026-03-07 20:56:28 +0530

    feat:add 10 minute cart feature

--- src/server.js
+++ src/server.js
@@ -23,7 +23,12 @@ app.post('/api/book',async (req,res) => {
         const findQuery=` 
             SELECT id, row_letter, seat_number 
             FROM seats 
-            WHERE event_id = $1 AND status = 'AVAILABLE' 
+            WHERE event_id=$1
+            AND
+             ( status='AVAILABLE'
+              OR
+              (status='HELD' AND expire_time <=NOW())
+              ) 
             LIMIT 1 
             FOR UPDATE SKIP LOCKED
         `;
@@ -34,19 +39,30 @@ app.post('/api/book',async (req,res) => {
         }
         const seat=rows[0];
 
+        const holdDuration=10;
+        const expire_time=new Date(Date.now()+holdDuration*60000);
+
         const upadateQuery=`
         UPDATE seats
-        SET status='SOLD' ,holder_id=$1
-        WHERE id=$2
+        SET status='HELD' ,holder_id=$1,expire_time=$2
+        WHERE id=$3
         RETURNING *
         ` 
-        await client.query(upadateQuery,[userID,seat.id]);
+        await client.query(upadateQuery,[userID,expire_time,seat.id]);
         
         await client.query('COMMIT');
 
         res.json({
-            message: "🎟️ Ticket booked successfully!",
-            seat: `Row ${seat.row_letter}, Seat ${seat.seat_number}`
+            message: "Ticket is in cart!",
+            seat: `Row ${seat.row_letter}, Seat ${seat.seat_number}`,
+            status:`Held`,
+            expiresAt: expire_time.toLocaleString(undefined, { 
+                weekday: 'short', 
+                hour: 'numeric', 
+                minute: '2-digit', 
+                hour12: true 
+            }),
+            instructions:`You have ${holdDuration} minutes to complete payment`
         });
     }
     catch(error) {
@@ -61,6 +77,25 @@ app.post('/api/book',async (req,res) => {
 
 const PORT= process.env.API_PORT || 3000;
 
+setInterval(async () => {
+    try {
+        const releaseQuery = `
+            UPDATE seats 
+            SET status = 'AVAILABLE', holder_id = NULL, hold_expires_at = NULL 
+            WHERE status = 'HELD' AND hold_expires_at <= NOW()
+            RETURNING id, row_letter, seat_number;
+        `;
+        
+        const { rows } = await db.pool.query(releaseQuery);
+        
+        if (rows.length > 0) {
+            console.log(` Released ${rows.length} expired seats back to the public pool.`);
+            rows.forEach(seat => console.log(`   - Freed up Row ${seat.row_letter}, Seat ${seat.seat_number}`));
+        }
+    } catch (error) {
+        console.error("Unkown Release Error", error);
+    }
+}, 60000);
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}`);
 });
